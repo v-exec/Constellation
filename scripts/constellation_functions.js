fb86e1cff862c6169d289e4869b4826621b4849a
Vi <reducedprocess@gmail.com>
2016-10-23 01:14:46 -0400

    clarifying documentation

--- scripts/constellation_functions.js
+++ scripts/constellation_functions.js
@@ -11,7 +11,7 @@ function getRandomFloat(min, max) {
 	return Math.random() * (max - min) + min;
 }
 
-//check if two dots' X and Y coordinates are closer than lineDistance
+//check if distance between two coordinates is closer than distanceComparator
 function distanceVerifier(dot1X, dot1Y, dot2X, dot2Y, distanceComparator) {
 	return ((Math.abs(dot1X - dot2X) < distanceComparator) && (Math.abs(dot1Y - dot2Y) < distanceComparator));
 }
